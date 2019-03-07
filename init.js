71ec086c5074997471af69378e26f187da8de217
ClaudinhoPedrotti <claudio.brito9@etec.sp.gov.br>
2019-03-07 20:17:30 -0300

    Primeira parte

---
+++ init.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    $('.tap-target').tapTarget('open');
+    $('.tap-target').tapTarget('close');
+
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({ fullWidth: true });
+    $('.button-collapse').sideNav({
+        menuWidth: 310, // Default is 300
+        edge: 'left', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+    }
+    );
+});
+
+var body = document.querySelector('body');
+var btnDark = document.querySelector("#modo-dark");
+btnDark.addEventListener("click", function (event) {
+
+    var backgroundColor = body.style.backgroundColor;
+
+    event.preventDefault();
+    body.style.backgroundColor = "grey";
+
+    if (backgroundColor == 'grey') {
+        body.style.backgroundColor = "white";
+    }
+});
+
